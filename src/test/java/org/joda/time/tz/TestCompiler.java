a6baeda58b8f49c89eb79d7fdf885b6b135eb986
Matthew Ly <mmly2@cs498dm-31a.cs.illinois.edu>
2015-04-21 21:47:19 -0500

    ZomeInfoCompiler Main function coverage

--- src/test/java/org/joda/time/tz/TestCompiler.java
+++ src/test/java/org/joda/time/tz/TestCompiler.java
@@ -117,6 +117,10 @@ public class TestCompiler extends TestCase {
         TestBuilder.testForwardTransitions(tz, TestBuilder.AMERICA_LOS_ANGELES_DATA);
         TestBuilder.testReverseTransitions(tz, TestBuilder.AMERICA_LOS_ANGELES_DATA);
     }
+	
+	public void testCompileMainZeroArguments() throws Exception {
+		ZoneInfoCompiler.main();
+	}
 
     private Provider compileAndLoad(String data) throws Exception {
         File tempDir = createDataFile(data);
